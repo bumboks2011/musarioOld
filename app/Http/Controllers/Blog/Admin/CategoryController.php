2bd80e9b9babd66ed312e08bb3761ae783732306
BuildTools <unconfigured@null.spigotmc.org>
2019-09-05 19:27:25 +0300

    add CRUD for categories

---
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use Illuminate\Http\Request;
+use App\Models\BlogCategory;
+
+class CategoryController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $paginator = BlogCategory::paginate(5);
+
+        return view('blog.admin.categories.index', compact('paginator'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $item = BlogCategory::findOrFail($id);
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = BlogCategory::find($id);
+        if (empty($item)) {
+            return back()->withErrors(['msg' => "Запись id=[{$id}] не найдена"])->withInput();
+        }
+
+        $data = $request->all();
+        $result = $item->fill($data)->save();
+
+        if($result) {
+            return redirect()->route('blog.admin.categories.edit', $item->id)->with(['success' => 'Успешно сохранено']);
+        } else {
+            back()->withErrors(['msg' => "Ошибка сохранения"])->withInput();
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', 'PostController')->names('blog.posts');
 });
 
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    // BlogCategory
+    $methods = ['index','edit','store','update','create'];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
+
 //Route::resource('rest', 'RestTestController')->names('restTest');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
