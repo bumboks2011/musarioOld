233983ac4b5dded6c92170779799419d1bb8558a
BuildTools <unconfigured@null.spigotmc.org>
2019-09-10 15:49:30 +0300

    add observers for Post

---
+++ app/Observers/BlogPostObserver.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\BlogPost;
+use Carbon\Carbon;
+
+class BlogPostObserver
+{
+    /**
+     * Handle the blog post "creating" event.
+     *
+     * @param  BlogPost  $blogPost
+     * @return void
+     */
+    public function creating(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the blog post "created" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function created(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the blog post "updating" event.
+     *
+     * @param  BlogPost  $blogPost
+     * @return void
+     */
+    public function updating(BlogPost $blogPost)
+    {
+        $this->setPublishedAt($blogPost);
+
+        $this->setSlug($blogPost);
+    }
+
+    /**
+     * If publish data not set and flag = Published, set publish data for now.
+     * @param BlogPost $blogPost
+     */
+    protected function setPublishedAt(BlogPost $blogPost)
+    {
+        if (empty($blogPost->published_at) && $blogPost->is_published) {
+            $blogPost->published_at = Carbon::now();
+        }
+    }
+
+    protected function setSlug(BlogPost $blogPost)
+    {
+        if (empty($blogPost->slug)) {
+            $blogPost->slug = \Str::slug($blogPost->title);
+        }
+    }
+
+    /**
+     * Handle the blog post "updated" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function updated(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the blog post "deleted" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function deleted(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the blog post "restored" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function restored(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the blog post "force deleted" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function forceDeleted(BlogPost $blogPost)
+    {
+        //
+    }
+}
